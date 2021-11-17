95339cf99a238795f310627b912982701bbd1d84
Mykhailo Sobko <m.t.sobko@gmail.com>
2021-11-17 19:37:43 +0200

    Add MessageSender

--- src/main/java/sender/MessageSender.java
+++ src/main/java/sender/MessageSender.java
@@ -3,11 +3,27 @@ package sender;
 import facebook.FacebookUser;
 import twitter.TwitterUser;
 
+import java.util.Date;
+
 public class MessageSender {
     public void send(String text, Object user, String country) {
         if (user instanceof FacebookUser) {
-
-        } if (user instanceof TwitterUser) {
+            if (
+                    ((FacebookUser) user).getUserActiveTime().getTime()
+                    < new Date().getTime() - 60*60*1e3
+                    && ((FacebookUser) user).getUserCountry().equals(country)
+            ) {
+                System.out.println("Message \"" + text + "\" sent\n");
+            }
+        }
+        if (user instanceof TwitterUser) {
+            if (
+                    ((TwitterUser) user).getLastActiveTime().getTime()
+                            < new Date().getTime() - 60*60*1e3
+                            && ((TwitterUser) user).getCountry().equals(country)
+            ) {
+                System.out.println("Message \"" + text + "\" sent\n");
+            }
         }
     }
 }
--- src/test/java/sender/MessageSenderTest.java
+++ src/test/java/sender/MessageSenderTest.java
@@ -1,6 +1,31 @@
 package sender;
 
-import static org.junit.jupiter.api.Assertions.*;
-class MessageSenderTest {
-  
+import facebook.FacebookUser;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import twitter.TwitterUser;
+
+import java.util.Date;
+
+public class MessageSenderTest {
+    private MessageSender messageSender;
+    private FacebookUser facebookUser;
+    private TwitterUser twitterUser;
+
+    @Test
+    public void send() {
+        messageSender = new MessageSender();
+        facebookUser = new FacebookUser(
+                "example@mail.com",
+                "Country",
+                new Date((long) (new Date().getTime() - 120*60*1e3))
+        );
+        twitterUser = new TwitterUser(
+                "example@mail.com",
+                "Country",
+                new Date((long) (new Date().getTime() - 120*60*1e3))
+        );
+        messageSender.send("example text", facebookUser, "Country");
+        messageSender.send("example text", twitterUser, "Country");
+    }
 }
